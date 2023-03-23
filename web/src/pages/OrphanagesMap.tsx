6c129cdca3bc42684424891d5601e7e7b0c912dd
bhenrique16 <bh.henrique16@gmail.com>
2023-03-23 03:36:43 -0300

    estilização pages e map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,37 @@
+import {Link} from 'react-router-dom'
+import {FiPlus} from 'react-icons/fi'
+import { Map } from 'react-leaflet'
 
 
+import mapMarkerImg from '../images/map-maker.svg'
+import '../styles/pages/orphanages-map.css'
 function OrphanagesMap(){
     return(
-        <h1>hello</h1>
+        <div id="page-map">
+            <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Logo Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita</p>
+        </header>
+
+        <footer>
+          <strong>Sobradinho</strong>
+          <span>Brasilia</span>
+        </footer>
+      </aside>
+            
+            <Map 
+            center={[-15.6417041,-47.8131106]}
+            zoom={15}
+           
+            />
+
+            <Link to="" className="create-orphanage" >
+                <FiPlus size={32} color="#FFF" /> 
+            </Link>
+        </div>
     )
 }
 
