edbe5dd9781cf920ac2a60b927d77ac186572588
bhenrique16 <bh.henrique16@gmail.com>
2023-03-24 03:35:35 -0300

    map

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,7 +1,8 @@
 import {Link} from 'react-router-dom'
 import {FiPlus} from 'react-icons/fi'
-import { Map } from 'react-leaflet'
+import { MapContainer,TileLayer } from 'react-leaflet'
 
+import 'leaflet/dist/leaflet.css'
 
 import mapMarkerImg from '../images/map-maker.svg'
 import '../styles/pages/orphanages-map.css'
@@ -21,12 +22,14 @@ function OrphanagesMap(){
           <span>Brasilia</span>
         </footer>
       </aside>
-            
-            <Map 
+      <MapContainer 
             center={[-15.6417041,-47.8131106]}
             zoom={15}
-           
-            />
+              style={{width:'100%',height:'100%'}}
+            >
+              <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </MapContainer>
+      
 
             <Link to="" className="create-orphanage" >
                 <FiPlus size={32} color="#FFF" /> 
